db1dd000393c52cad6953a0089835994c32abe5a
DDumas551 <david.j.dumas@gmail.com>
2019-12-18 15:43:21 -0800

    More Dbly Linked List

--- UdemyNotes/JsAlgorithmsAndDataStructuresMasterclass/Section20DoublyLinkedLists/DoublyLinkedLists.js
+++ UdemyNotes/JsAlgorithmsAndDataStructuresMasterclass/Section20DoublyLinkedLists/DoublyLinkedLists.js
@@ -1,8 +1,8 @@
 class Node {
   constructor(val) {
     this.val = val;
-    this.next = next;
-    this.prev = prev;
+    this.next = null;
+    this.prev = null;
   }
 }
 
@@ -11,5 +11,93 @@ class DoublyLinkedList {
     this.head = null;
     this.tail = null;
     this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.head.prev = newNode;
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return undefined;
+    if (idx <= this.length / 2) {
+      let count = 0;
+      let current = this.head;
+      while (count !== idx) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      var count = this.length - 1;
+      var current = this.tail;
+      while (count !== idx) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+  set(idx, val) {
+    let foundNode = this.get(idx);
+    if (foundNode !== undefined) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(idx, val) {
+    if (idx < 0 || idx > this.length) return false;
+    if (idx === 0) this.unshift(val);
+    if (idx === this.length) this.push(val);
+    let foundNode = this.get(idx);
+    this.length++;
+    return true;
   }
 }
